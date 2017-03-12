28a4e96a29d83eb3b96dd1d72405b74d0e81cf27
Rony Langbord <rony.langbord@wework.com>
2017-03-12 15:58:36 +0200

    Changes on front end according to Sagi’s PR

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,7 +4,7 @@ class RestaurantsController < ApplicationController
   # GET /restaurants
   # GET /restaurants.json
   def index
-    @restaurants = Restaurant.all
+    @restaurants = Restaurant.all.order(:name)
   end
 
   # GET /restaurants/1
